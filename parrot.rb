b6507b026b1e40845e6c621e3638f1b6b6ce00f3
Pedro Bravo <pedro.bravoa@yahoo.com>
2020-04-21 21:10:06 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,10 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
+#Create method `parrot` that outputs a given phrase and
+#returns the phrase
+
+def parrot(phrase="Squawk!")
+  "Squawk!"
+  puts "Pretty bird!, #{phrase}"
+  return phrase
+end
+
+parrot
